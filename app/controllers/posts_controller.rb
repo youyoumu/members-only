671e5a0959be147be7d9aa9bf1bb8c662571549d
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-02-16 16:55:01 +0700

    Completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,36 @@
 class PostsController < ApplicationController
-  before_action :require_login, only: [:new, :create]
+  before_action :require_sign_in, only: [:new, :create]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   private
 
-  def require_login
+  def require_sign_in
+    unless user_signed_in?
+      authenticate_user!
+    else
+
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 
 end
